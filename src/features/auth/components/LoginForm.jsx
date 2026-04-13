9d944a5b4fc4f07d632d9b919c509602c43d5715
EddyCode1 <etucubal-2024505@kinal.edu.gt>
2026-04-13 14:52:00 -0600

    first commit

---
+++ src/features/auth/components/LoginForm.jsx
@@ -0,0 +1,92 @@
+import { useForm } from "react-hook-form";
+import { useAuthStore } from "../store/authStore";
+import { useNavigate } from 'react-roter-dom';
+ 
+export const LoginForm = ({ onForgot }) => {
+  const login = useAuthStore((state) => state.login);
+  const loading = useAuthStore((state) => state.loading);
+  const error = useAuthStore((state) => state.error);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+ 
+  const onSubmit = async (data) => {
+    //Mandar informacion al backend para iniciar sesion
+    console.log(data);
+    const res = await login(data);
+  };
+ 
+  return (
+    <form onSubmit={handleSubmit(onSubmit)} className="space-y-5">
+      <div>
+        <label
+          htmlFor="emailOrUsername"
+          className="block text-sm font-medium text-gray-800 mb-1.5"
+        >
+          Email o Usuario
+        </label>
+ 
+        <input
+          id="emailOrUsername"
+          type="text"
+          className="w-full px-3 py-2 text-sm border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500"
+          placeholder="correo@ejemplo-com o usuario"
+          {...register("emailOrUsername", {
+            required: "este campo es obligatorio",
+          })}
+        />
+ 
+        {errors.emailOrUsername && (
+          <p className="text-red-600 text-xs mt-1">
+            {errors.emailOrUsername.message}
+          </p>
+        )}
+      </div>
+      <div>
+        <label
+          htmlFor="password"
+          className="block text-sm font-medium text-gray-800 mb-1.5"
+        >
+          Contraseña
+        </label>
+ 
+        <input
+          id="password"
+          type="password"
+          placeholder="......."
+          className="w-full px-3 py-2 text-sm border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500"
+          {...register("password", {
+            required: "La contraseña es obligatoria",
+          })}
+        />
+ 
+        {errors.emailOrUsername && (
+          <p className="text-red-600 text-xs mt-1">{errors.password.message}</p>
+        )}
+      </div>
+ 
+        {error && <p className="text-red-600 text-sx mt-1">{error}</p>}
+ 
+      <button
+        type="submit"
+        className="w-full bg-main-blue hover:opacity-90 text-white font medium py-2.5 px-4 rounded-lg
+                    transition-colors duration-200 text-sm disabled:opacity-50"
+        disabled={loading}
+      >
+        {loading ? "Iniciando..." : "Iniciar sesión"}
+      </button>
+      <p className="text-center text-sm">
+        <button
+          type="button"
+          onClick={onForgot}
+          className="text-main-blue hover:underline"
+        >
+          ¿Olvidaste tu contraseña?
+        </button>
+      </p>
+    </form>
+  );
+};
+ 
---
+++ src/features/auth/store/authStore.js
@@ -0,0 +1,40 @@
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+import { login as loginRequest } from "../../../shared/api";
+
+export const useAuthStore = create(
+    persist(
+        (set, get) => ({
+            user: null,
+            token: null,
+            expiresAt: null,
+            loading: false,
+            error: null, 
+            isAuthenticated: false, 
+
+            login: async (emailOrUsername, password) => {
+                try {
+                    set({loading: true, error: null});
+
+                const { data } = await loginRequest({emailOrUsername, password});
+                
+                set({
+                    user: data.userDetails, 
+                    token: data.token,
+                    expiresAt: data.expiresAt,
+                    loading: false,
+                })
+                return { success: true}
+
+                } catch (err) {
+                    console.error("Login error: ", error);
+                    const message =
+                        err.response?.data?.message || "Error de autenticacion" ;
+                    set({ error: message, loading: false})
+                    return { success: false, error: message}
+                }
+            }
+        }),
+        {name:  "auth-storage"}
+    )
+)
